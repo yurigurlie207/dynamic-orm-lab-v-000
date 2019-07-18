5db9e30615d4c2ac6378af762456a557879ee8ed
Adrienne Yao <adrienne.yao@icloud.com>
2019-07-18 14:02:35 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -20,8 +20,8 @@ class InteractiveRecord
   column_names.compact
 end
 
-def table_name_for_insert
-  self.class.table_name
-end
+  def table_name_for_insert
+    self.class.table_name
+  end
 
 end
