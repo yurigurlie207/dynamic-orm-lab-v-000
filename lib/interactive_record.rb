ec7c04eb5c51bda6eff12d3f3ee88f5b1a09ae7d
Adrienne Yao <adrienne.yao@icloud.com>
2019-07-18 14:03:07 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,6 +2,11 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+
+  def self.table_name
+    "#{self.to_s.downcase}s"
+  end
+  
   def initialize(options={})
    options.each do |property, value|
      self.send("#{property}=", value)
@@ -23,5 +28,7 @@ class InteractiveRecord
   def table_name_for_insert
     self.class.table_name
   end
+
+  
 
 end
