128ee92e05e05e79006e9684ddd245669a29c9ac
Adrienne Yao <adrienne.yao@icloud.com>
2019-07-18 14:14:00 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,12 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
+
 end
