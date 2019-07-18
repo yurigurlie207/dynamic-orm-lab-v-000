79e29243d46027184aea7213930ef32280017ac7
Adrienne Yao <adrienne.yao@icloud.com>
2019-07-18 13:37:41 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,6 +2,12 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+  def initialize(options={})
+   options.each do |property, value|
+     self.send("#{property}=", value)
+   end
+ end
+ 
   def self.column_names
 
   sql = "pragma table_info('#{table_name}')"
