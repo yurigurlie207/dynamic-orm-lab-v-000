5ebbb1ea43cefc3456cae57477650ca86f8db2b7
Adrienne Yao <adrienne.yao@icloud.com>
2019-07-18 14:09:44 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -41,6 +41,10 @@ class InteractiveRecord
    values.join(", ")
   end
 
-
+  def save
+   sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+   DB[:conn].execute(sql)
+   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+ end
 
 end
